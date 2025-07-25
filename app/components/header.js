18209ea31b7408b5e30abc4c85eb43a5ffd4c5dd
Shalom Taiwo <ifeshalomt@gmail.com>
2025-07-25 13:48:22 +0200

    Header and mobile menu

--- app/components/header.js
+++ app/components/header.js
@@ -1,8 +1,70 @@
 import React from 'react'
+import Image from "next/image";
+import {Menu} from "lucide-react";
 
 const Header = () => {
+
+    const [open, setOpen] = React.useState(false);
     return (
-        <div>Header</div>
+        <div className={'absolute z-10 top-12 lg:left-10 lg:w-auto w-full'}>
+            <div className={'lg:flex hidden items-center justify-center space-x-12'}>
+                <div>
+                    <Image src={'./room.svg'} alt={'logo'} width={80} height={50} objectFit={'cover'} />
+                </div>
+                <nav aria-label="menu">
+                    <ul className={'flex items-center space-x-10 text-base text-white font-semibold leading-none'}>
+                        <li>
+                            home
+                        </li>
+                        <li>
+                            shop
+                        </li>
+                        <li>
+                            about
+                        </li>
+                        <li>
+                            contact
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+            <div className={'lg:hidden '}>
+                <div className={'flex items-start justify-between w-full px-6'}>
+                    <button className={'slide-enter cursor-pointer'} onClick={() => {
+                        setOpen(!open);
+                    }}>
+                        <Menu color={'white'} />
+                    </button>
+                    <div>
+                        <Image src={'./room.svg'} alt={'logo'} width={80} height={50} objectFit={'contain'} />
+                    </div>
+
+                    <div>
+                        <p></p>
+                    </div>
+                </div>
+                <div className={`p-6 ${open ? 'slide-enter block' : 'hidden'}`}>
+                    <nav aria-label="menu" className={'w-full'}>
+                        <ul className={'flex flex-col items-center space-y-6 py-3 text-base text-black font-semibold leading-none bg-white w-full'}>
+                            <li>
+                                home
+                            </li>
+                            <li>
+                                shop
+                            </li>
+                            <li>
+                                about
+                            </li>
+                            <li>
+                                contact
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+        </div>
     )
 }
 export default Header
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import {ChevronLeft, ChevronRight, MoveRight} from "lucide-react";
 import {useState} from "react";
+import Header from "@/app/components/header";
 
 export default function Home() {
     const [sliderNo, setSliderNo] = useState(0);
@@ -49,6 +50,7 @@ export default function Home() {
     <div className="overflow-x-hidden w-full h-full flex flex-col items-center justify-center bg-white relative">
       <div className={'flex lg:flex-row flex-col lg:h-[67.5vh] md:h-[130vh] h-screen w-full'}>
         <div className={'relative lg:w-[60%] w-full h-full'}>
+            <Header />
             <div className={`${isAnimating ? 'slide-exit' : 'slide-enter'}`}>
                 {sliderSwitch()}
             </div>
@@ -62,8 +64,8 @@ export default function Home() {
             </div>
         </div>
         <div className={'relative lg:w-[40%] w-full h-full'}>
-            <div className={'bg-white pt-0 px-12 flex flex-col content-center justify-center space-y-5 h-full'}>
-                <h2 className={'font-semibold text-5xl text-black leading-10'}>Discover innovative ways to decorate</h2>
+            <div className={'bg-white pt-0 lg:px-12 px-10 flex flex-col content-center justify-center space-y-5 h-full'}>
+                <h2 className={'font-semibold md:text-5xl text-4xl text-black md:leading-10 leading-8'}>Discover innovative ways to decorate</h2>
                 <p className={'text-base leading-snug text-gray-500 font-medium'}>
                     We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.
                 </p>
@@ -86,7 +88,7 @@ export default function Home() {
         <div className={'h-74 relative overflow-hidden lg:w-[25%] w-full'}>
             <Image src={'/furn.png'} alt={'furn.png'} fill={true} objectFit={'cover'} />
         </div>
-        <div className={'bg-white lg:w-[42%] w-full lg:h-74 h-full relative overflow-hidden lg:py-4 py-16 lg:px-12 px-16 flex flex-col content-center justify-center'}>
+        <div className={'bg-white lg:w-[42%] w-full lg:h-74 h-full relative overflow-hidden lg:py-4 py-16 lg:px-12 md:px-16 px-10 flex flex-col content-center justify-center'}>
                 <h3 className={'font-bold text-base leading-snug text-black tracking-[7px] mb-4'}>ABOUT OUR FURNITURE</h3>
                 <p className={'text-base leading-snug text-gray-500 font-medium'}>
                     Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.
